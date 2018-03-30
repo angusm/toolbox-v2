ee9e8831b3a689f374ec3c303f719e881fa672ed
Angus MacDonald <angusamacdonald@hotmail.com>
2018-03-30 19:50:34 -0400

    Added getValuesFromInputs()

---
+++ src/toolbox/utils/dom/form/get-values-from-inputs.ts
@@ -0,0 +1,15 @@
+import {getAsList} from '../../object/get-as-list';
+import {getAttributeValue} from '../get-attribute-value';
+import {getInputValue} from './get-input-value';
+
+function getValuesFromInputs(inputs: NodeList) {
+  return getAsList(inputs)
+    .reduce(
+      (result: {[s: string]: string}, input: HTMLInputElement) => {
+        const inputName = getAttributeValue(input, 'name');
+        result[inputName] = getInputValue(input);
+        return result;
+      }, {});
+}
+
+export {getValuesFromInputs};
