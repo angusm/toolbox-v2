c9534ce7de729379c550189397329c0d1111c748
Angus MacDonald <angusamacdonald@hotmail.com>
2018-04-01 11:32:17 -0400

    FrameMemoized getAncestorDimensions

--- src/toolbox/utils/dom/position/get-ancestor-dimensions.ts
+++ src/toolbox/utils/dom/position/get-ancestor-dimensions.ts
@@ -1,11 +1,14 @@
 import {Dimensions2d} from "../../math/geometry/dimensions-2d";
+import {frameMemoize} from "../../frame-memoize";
 
-function getAncestorDimensions(ancestor: HTMLElement = null): Dimensions2d {
+function getAncestorDimensions_(ancestor: HTMLElement = null): Dimensions2d {
   if (ancestor) {
     return Dimensions2d.fromElementOffset<Dimensions2d>(ancestor);
   } else {
     return Dimensions2d.fromInnerWindow<Dimensions2d>();
   }
 }
+
+const getAncestorDimensions = frameMemoize(getAncestorDimensions_);
 
 export {getAncestorDimensions};
