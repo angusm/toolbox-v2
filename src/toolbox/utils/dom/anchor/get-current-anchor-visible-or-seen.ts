b0a8f70401ac9b06875e2e5749ff59a74faa9824
Angus MacDonald <angusamacdonald@hotmail.com>
2018-04-01 17:51:30 -0400

    Added getCurrentAnchorVisibleOrSeen

---
+++ src/toolbox/utils/dom/anchor/get-current-anchor-visible-or-seen.ts
@@ -0,0 +1,31 @@
+import {isFullyVisible} from '../position/is-fully-visible';
+import {CommonSelector} from "../common-selector";
+import {frameMemoize} from "../../frame-memoize";
+import {getDistanceUntilVisible} from "../position/get-distance-until-visible";
+import {min} from "../../array/min";
+
+function getCurrentAnchorVisibleOrSeen_(
+  querySelector: string = CommonSelector.DEEP_LINK_TARGETS
+): Node {
+  const hash = window.location.hash;
+  if (hash) {
+    const anchorElement: Node = document.querySelector(hash);
+    if (anchorElement && isFullyVisible(<HTMLElement>anchorElement)) {
+      return anchorElement;
+    }
+  }
+  const anchors: HTMLElement[] =
+    <HTMLElement[]>Array.from(document.querySelectorAll(querySelector));
+  const eligibleAnchors: HTMLElement[] =
+    anchors.filter((anchor) => getDistanceUntilVisible(anchor).y <= 0);
+
+  //noinspection JSSuspiciousNameCombination
+  return min(eligibleAnchors, (a) => Math.abs(getDistanceUntilVisible(a).y));
+}
+
+// Frame memoize as it is likely this will be used by both DeepLinkByScroll and
+// IDMarker
+const getCurrentAnchorVisibleOrSeen =
+  frameMemoize(getCurrentAnchorVisibleOrSeen_);
+
+export {getCurrentAnchorVisibleOrSeen};
