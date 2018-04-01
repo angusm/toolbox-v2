3e2d19622ae419c591f76263300e706b4f0f0e0f
Angus MacDonald <angusamacdonald@hotmail.com>
2018-04-01 11:28:34 -0400

    Fixed isScrolledPast using length instead of Y

--- src/toolbox/utils/dom/position/is-scrolled-past.ts
+++ src/toolbox/utils/dom/position/is-scrolled-past.ts
@@ -3,7 +3,7 @@ import {getDistanceUntilVisible} from "./get-distance-until-visible";
 function isScrolledPast(
   element: HTMLElement, container: HTMLElement = null
 ): boolean {
-  return getDistanceUntilVisible(element, container).getLength() < 0;
+  return getDistanceUntilVisible(element, container).y < 0;
 }
 
 export {isScrolledPast};
