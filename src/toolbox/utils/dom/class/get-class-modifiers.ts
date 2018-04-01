e71a6d06df1e16b525f600ba2fd8aee7b386b048
Angus MacDonald <angusamacdonald@hotmail.com>
2018-04-01 19:21:50 -0400

    Fixed bug with nested modifiers in CSS for getClassModifiers

--- src/toolbox/utils/dom/class/get-class-modifiers.ts
+++ src/toolbox/utils/dom/class/get-class-modifiers.ts
@@ -1,7 +1,8 @@
 function getClassModifiers(element: Element, baseClass: string): string[] {
+  const base: string = `${baseClass}--`;
   return Array.from(element.classList)
-    .filter((candidate) => candidate.indexOf(`${baseClass}--`) === 0)
-    .map((matchingClass) => matchingClass.split('--').slice(1).join('--'));
+    .filter((candidate) => candidate.indexOf(base) === 0)
+    .map((matchingClass) => matchingClass.split(base).slice(1).join(base));
 }
 
 export {getClassModifiers};
