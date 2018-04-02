82a0040d12a9efd6b0b2e42473e0b9d84b05c16b
Angus MacDonald <angusamacdonald@hotmail.com>
2018-04-01 21:47:28 -0400

    Added getCurrentAnchorFromLimitedSet

---
+++ src/toolbox/utils/dom/anchor/get-current-anchor-from-limited-set.ts
@@ -0,0 +1,34 @@
+import {getClosestToCenter} from '../position/get-closest-to-center';
+import {frameMemoize} from "../../frame-memoize";
+import {isAbove} from "../position/is-above";
+
+type getAnchorFn = (querySelector: string) => Node;
+
+function getCurrentAnchorFromLimitedSet_(
+  limitedQuerySelector: string, getCurrentAnchor: getAnchorFn
+): getAnchorFn {
+  return frameMemoize(function(querySelector: string) {
+    const baseResult: HTMLElement =
+      <HTMLElement>getCurrentAnchor(querySelector);
+    const limitedCandidates: HTMLElement[] =
+      <HTMLElement[]>
+        Array.from(document.querySelectorAll(limitedQuerySelector));
+
+    if (limitedCandidates.indexOf(baseResult) !== -1) {
+      return baseResult;
+    }
+
+    const resultsAboveBase: HTMLElement[] =
+      limitedCandidates.filter((candidate) => isAbove(candidate, baseResult));
+
+    return getClosestToCenter(resultsAboveBase);
+  });
+}
+
+// Frame memoize as it is likely this will be used by both DeepLinkByScroll and
+// IDMarker
+const getCurrentAnchorFromLimitedSet:
+  (s: string, f: getAnchorFn) => getAnchorFn =
+  frameMemoize(getCurrentAnchorFromLimitedSet_);
+
+export {getCurrentAnchorFromLimitedSet};
