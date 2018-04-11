ea44f23db148c797e1d144d74b6782c504ca2a85
Angus MacDonald <angusamacdonald@hotmail.com>
2018-04-11 18:15:02 -0400

    Added inactive class to progressive disclosure

--- src/toolbox/components/progressive-disclosure/metered.ts
+++ src/toolbox/components/progressive-disclosure/metered.ts
@@ -2,41 +2,56 @@ import {zip} from "../../utils/array/zip";
 import {eventHandler} from "../../utils/event/event-handler";
 import {addClassIfMissing} from "../../utils/dom/class/add-class-if-missing";
 import {SeenBottomForDuration} from "../../utils/event/events/seen-bottom-for-duration";
+import {removeClassModifiers} from "../../utils/dom/class/remove-class-modifiers";
 
 interface ICSSClassOptions {
   base?: string;
-  modifier?: string;
+  activeModifier?: string;
+  inactiveModifier?: string;
 }
 
 const DefaultClassOptions = Object.freeze({
   base: 'tb-metered-progressive-disclosure',
-  modifier: 'active',
+  activeModifier: 'active',
+  inactiveModifier: 'inactive',
 });
 
 class MeteredProgressiveDisclosure {
   private targets_: HTMLElement[];
   private threshold_: number;
   private baseClass_: string;
-  private modifier_: string;
+  private activeModifier_: string;
+  private inactiveModifier_: string;
 
   constructor(
     targets: HTMLElement[],
     threshold: number,
     {
       base = DefaultClassOptions.base,
-      modifier = DefaultClassOptions.modifier,
+      activeModifier = DefaultClassOptions.activeModifier,
+      inactiveModifier = DefaultClassOptions.inactiveModifier,
     }: ICSSClassOptions,
   ) {
     this.targets_ = targets;
     this.threshold_ = threshold;
     this.baseClass_ = base;
-    this.modifier_ = modifier;
+    this.activeModifier_ = activeModifier;
+    this.inactiveModifier_ = inactiveModifier;
     this.init_();
   }
 
   private init_(): void {
     this.targets_
-      .forEach((target) => addClassIfMissing(target, this.baseClass_));
+      .forEach((target: HTMLElement, index: number) => {
+        addClassIfMissing(target, this.baseClass_);
+        if (index !== 0) {
+          addClassIfMissing(target, this.getInactiveCSSClass_());
+          removeClassModifiers(
+            target, this.baseClass_, {whitelist: [this.activeModifier_]});
+        }
+      });
+
+    addClassIfMissing(this.targets_[0],
 
     const shiftedTargets: HTMLElement[] = [null, ...this.targets_];
     const targetsWithNext: HTMLElement[][] =
@@ -48,14 +63,22 @@ class MeteredProgressiveDisclosure {
         eventHandler.addListener(
           target,
           this.getSeenForDurationClass_(),
-          () => addClassIfMissing(next, this.getActiveCSSClass_())
+          () => {
+            addClassIfMissing(next, this.getActiveCSSClass_());
+            removeClassModifiers(
+              next, this.baseClass_, {whitelist: [this.inactiveModifier_]});
+          }
         );
       }
     )
   }
 
   private getActiveCSSClass_() {
-    return `${this.baseClass_}--${this.modifier_}`;
+    return `${this.baseClass_}--${this.activeModifier_}`;
+  }
+
+  private getInactiveCSSClass_() {
+    return `${this.baseClass_}--${this.inactiveModifier_}`;
   }
 
   private getSeenForDurationClass_() {
