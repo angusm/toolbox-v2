ec5506cef8ac0a65101ae738194daa1efda386c7
Angus MacDonald <angusamacdonald@hotmail.com>
2018-04-11 22:30:16 -0400

    Testing rework of getVisibleDistanceFromRoot

--- src/toolbox/utils/dom/position/get-visible-distance-from-root.ts
+++ src/toolbox/utils/dom/position/get-visible-distance-from-root.ts
@@ -6,31 +6,20 @@ import {getStyle} from "../style/get-style";
 const scroll: Scroll = Scroll.getSingleton();
 const ZERO_VECTOR: Vector2d = new Vector2d();
 
-function getVisibleDistanceFromRoot__(element: HTMLElement): Vector2d {
+function getVisibleDistanceFromRoot_(element: HTMLElement): Vector2d {
   if (!element || element === document.body) {
     return ZERO_VECTOR;
   } else if (getStyle(element, 'position') === 'fixed') {
-    return Vector2d.fromElementOffset(element);
+    return Vector2d.fromElementOffset(element) + scroll.getPosition().invert();
   } else {
     return Vector2d.add<Vector2d>(
       Vector2d.fromElementOffset(element),
       Vector2d.fromElementTransform(element),
       Vector2d.fromElementScroll(element).invert(),
-      memoized_(element.offsetParent));
+      memoized(element.offsetParent));
   }
-}
-
-function getVisibleDistanceFromRoot_(element: HTMLElement): Vector2d {
-  if (getStyle(element, 'position') === 'fixed') {
-    return Vector2d.fromElementOffset(element);
-  }
-  return Vector2d.add<Vector2d>(
-    scroll.getPosition().invert(),
-    Vector2d.fromElementScroll(element),
-    memoized_(element));
 }
 
 const memoized = frameMemoize(getVisibleDistanceFromRoot_);
-const memoized_ = frameMemoize(getVisibleDistanceFromRoot__);
 
 export {memoized as getVisibleDistanceFromRoot};
