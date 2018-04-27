641cc92541ce0238907a47059708c5c9e3fee355
Angus MacDonald <angusamacdonald@hotmail.com>
2018-04-27 16:14:38 -0400

    getElementBehind now uses visible area to determine winner

--- src/toolbox/utils/dom/position/get-element-behind.ts
+++ src/toolbox/utils/dom/position/get-element-behind.ts
@@ -1,22 +1,11 @@
 import {frameMemoize} from '../../frame-memoize';
-import {min} from "../../array/min";
-import {getDistanceBetweenCenters} from "./get-distance-between-centers";
-import {Dimensions2d} from "../../math/geometry/dimensions-2d";
+import {getVisibleArea} from "./get-visible-area";
+import {max} from "../../array/max";
 
 function getElementBehind_(
   target: HTMLElement, candidates: HTMLElement[]
 ): HTMLElement {
-    const candidatesBehindElement: HTMLElement[] =
-      candidates.filter(
-        (candidate) => {
-          const dimensions = Dimensions2d.fromElementOffset(candidate);
-          const distance = getDistanceBetweenCenters(target, candidate);
-          return distance.x <= dimensions.width / 2 &&
-              distance.y <= dimensions.height / 2;
-        });
-    return min(
-      candidatesBehindElement,
-      (candidate) => getDistanceBetweenCenters(target, candidate).getLength());
+    return max(candidates, (candidate) => getVisibleArea(target, candidate));
 }
 
 const getElementBehind = frameMemoize(getElementBehind_);
