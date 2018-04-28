b4711c8167003bcbab58359cd1235828c1df5de6
Angus MacDonald <angusamacdonald@hotmail.com>
2018-04-27 23:14:17 -0400

    Added HighContrastBorderColor

---
+++ src/toolbox/components/high-contrast-property/border-color.ts
@@ -0,0 +1,9 @@
+import {HighContrastProperty} from "./base";
+
+class HighContrastBorderColor extends HighContrastProperty {
+  protected static getProperty() {
+    return 'border-color';
+  }
+}
+
+export {HighContrastBorderColor};
