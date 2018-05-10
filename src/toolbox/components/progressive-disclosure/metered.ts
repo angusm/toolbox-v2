4fea94d98fbd8806392c27c6f6aeaf5f9dab1337
Angus MacDonald <angusamacdonald@hotmail.com>
2018-05-10 19:37:00 -0400

    Reworked Metered progressive disclosure

--- src/toolbox/components/progressive-disclosure/metered.ts
+++ src/toolbox/components/progressive-disclosure/metered.ts
@@ -4,6 +4,8 @@ import {addClassIfMissing} from "../../utils/dom/class/add-class-if-missing";
 import {SeenBottomForDuration} from "../../utils/event/events/seen-bottom-for-duration";
 import {removeClassIfPresent} from "../../utils/dom/class/remove-class-if-present";
 import {IsBottomVisible} from "../../utils/event/events/is-bottom-visible";
+import {noop} from "../../utils/noop";
+import {renderLoop} from "../../utils/render-loop";
 
 interface ICSSClassOptions {
   base?: string;
@@ -12,25 +14,24 @@ interface ICSSClassOptions {
   timerStartedModifier?: string;
   timerEndedModifier?: string;
   transitionTime?: number;
+  updateFunction?: (target: HTMLElement, next: HTMLElement, timeSeen: number, currentTime: number) => void;
 }
 
 const DefaultClassOptions = Object.freeze({
   base: 'tb-metered-progressive-disclosure',
   activeModifier: 'active',
   inactiveModifier: 'inactive',
-  timerStartedModifier: 'timer-started',
-  timerEndedModifier: 'timer-ended',
 });
 
 class MeteredProgressiveDisclosure {
-  private targets_: HTMLElement[];
   private threshold_: number;
   private baseClass_: string;
   private activeModifier_: string;
   private inactiveModifier_: string;
-  private timerStartedModifier_: string;
-  private timerEndedModifier_: string;
   private transitionTime_: number;
+  private updateFunction_: (target: HTMLElement, next: HTMLElement, timeSeen: number, currentTime: number) => void;
+  private timeBottomSeenByElement_: Map<HTMLElement, number>;
+  private nextTargets_: Map<HTMLElement, HTMLElement>;
 
   constructor(
     targets: HTMLElement[],
@@ -39,76 +40,105 @@ class MeteredProgressiveDisclosure {
       base = DefaultClassOptions.base,
       activeModifier = DefaultClassOptions.activeModifier,
       inactiveModifier = DefaultClassOptions.inactiveModifier,
-      timerStartedModifier = DefaultClassOptions.timerStartedModifier,
-      timerEndedModifier = DefaultClassOptions.timerEndedModifier,
       transitionTime = 0,
+      updateFunction = noop,
     }: ICSSClassOptions,
   ) {
-    this.targets_ = targets;
     this.threshold_ = threshold;
     this.baseClass_ = base;
     this.activeModifier_ = activeModifier;
     this.inactiveModifier_ = inactiveModifier;
-    this.timerStartedModifier_ = timerStartedModifier;
-    this.timerEndedModifier_ = timerEndedModifier;
     this.transitionTime_ = transitionTime;
-    this.init_();
+    this.updateFunction_ = updateFunction;
+    this.timeBottomSeenByElement_ = new Map();
+    this.nextTargets_ = new Map();
+
+    this.init_(targets);
   }
 
-  private init_(): void {
-    this.targets_
-      .forEach((target: HTMLElement, index: number) => {
-        addClassIfMissing(target, this.baseClass_);
-        if (index !== 0) {
-          addClassIfMissing(target, this.getInactiveCSSClass_());
-          removeClassIfPresent(this.targets_[0], this.getActiveCSSClass_());
-        }
+  private init_(targets: HTMLElement[]): void {
+    this.initTargetElements_(targets);
+    this.initNextTargets_(targets);
+    this.initSeenBottomListeners_();
+    this.update_();
+  }
+
+  private initTargetElements_(targets: HTMLElement[]): void {
+    targets
+      .forEach(
+        (target: HTMLElement) => addClassIfMissing(target, this.baseClass_));
+
+    targets
+      .slice(1)
+      .forEach((target) => {
+        addClassIfMissing(target, this.getInactiveCSSClass_());
+        removeClassIfPresent(target, this.getActiveCSSClass_());
       });
 
-    addClassIfMissing(this.targets_[0], this.getActiveCSSClass_());
-    removeClassIfPresent(this.targets_[0], this.getInactiveCSSClass_());
-
-    const shiftedTargets: HTMLElement[] = [null, ...this.targets_];
-    const targetsWithNext: HTMLElement[][] =
-      zip<HTMLElement>(shiftedTargets, this.targets_)
-        .slice(1, this.targets_.length);
-
-    this.setupSequence_(targetsWithNext, 0);
+    removeClassIfPresent(targets[0], this.getInactiveCSSClass_());
+    addClassIfMissing(targets[0], this.getActiveCSSClass_());
   }
 
-  private setupSequence_(
-    targetsWithNext: HTMLElement[][], transitionTime: number
-  ): void {
-    if (targetsWithNext.length === 0) {
+  private initNextTargets_(targets: HTMLElement[]) {
+    const shiftedTargets: HTMLElement[] = [null, ...targets];
+    const targetsWithNext: HTMLElement[][] =
+      zip<HTMLElement>(shiftedTargets, targets).slice(1, targets.length);
+    targetsWithNext.forEach(
+      ([target, next]) => this.nextTargets_.set(target, next));
+  }
+
+  private initSeenBottomListeners_() {
+    Array.from(this.nextTargets_.keys())
+      .forEach(
+        (target) => {
+          const seenBottomListener: number =
+            eventHandler.addListener(
+              target,
+              IsBottomVisible,
+              () => {
+                eventHandler.removeListener(seenBottomListener);
+                this.timeBottomSeenByElement_
+                  .set(target, <number>new Date().valueOf());
+              });
+        });
+  }
+
+  private isFullyDisclosed_() {
+    return this.getTargets_().length > 0;
+  }
+
+  private getTargets_() {
+    return Array.from(this.nextTargets_.keys());
+  }
+
+  private update_(): void {
+    if (!this.isFullyDisclosed_()) {
+      renderLoop.measure(() => renderLoop.cleanup(() => this.update_()));
+    }
+
+    this.getTargets_().forEach((target) => this.updateTarget_(target));
+  }
+
+  private updateTarget_(target: HTMLElement) {
+    if (!this.timeBottomSeenByElement_.has(target)) {
       return;
     }
 
-    const [target, next]: HTMLElement[] = targetsWithNext[0];
-    const remaining = targetsWithNext.slice(1);
+    const next = this.nextTargets_.get(target);
+    const currentTime = <number>new Date().valueOf();
+    const timeSeen = this.timeBottomSeenByElement_.get(target);
 
-    setTimeout(() => {
-      const seenBottomListener: number =
-        eventHandler.addListener(
-          target,
-          IsBottomVisible,
-          () => {
-            eventHandler.removeListener(seenBottomListener);
-            addClassIfMissing(target, this.getTimerStartedCSSClass_())
-          }
-        );
-      const seenForDurationListener: number =
-        eventHandler.addListener(
-          target,
-          this.getSeenForDurationClass_(),
-          () => {
-            eventHandler.removeListener(seenForDurationListener);
-            addClassIfMissing(target, this.getTimerEndedCSSClass_());
-            removeClassIfPresent(target, this.getTimerStartedCSSClass_());
-            addClassIfMissing(next, this.getActiveCSSClass_());
-            removeClassIfPresent(next, this.getInactiveCSSClass_());
-            this.setupSequence_(remaining, this.transitionTime_);
-          });
-    }, transitionTime);
+    if (currentTime > timeSeen + this.transitionTime_) {
+      this.nextTargets_.delete(target);
+      this.disclose_(next);
+    } else {
+      this.updateFunction_(target, next, timeSeen, currentTime);
+    }
+  }
+
+  private disclose_(elementToDisclose: HTMLElement) {
+    removeClassIfPresent(elementToDisclose, this.getInactiveCSSClass_());
+    addClassIfMissing(elementToDisclose, this.getActiveCSSClass_());
   }
 
   private getActiveCSSClass_() {
@@ -117,18 +147,6 @@ class MeteredProgressiveDisclosure {
 
   private getInactiveCSSClass_() {
     return `${this.baseClass_}--${this.inactiveModifier_}`;
-  }
-
-  private getTimerStartedCSSClass_() {
-    return `${this.baseClass_}--${this.timerStartedModifier_}`;
-  }
-
-  private getTimerEndedCSSClass_() {
-    return `${this.baseClass_}--${this.timerEndedModifier_}`;
-  }
-
-  private getSeenForDurationClass_() {
-    return SeenBottomForDuration.getClassforDuration(this.threshold_);
   }
 }
 
