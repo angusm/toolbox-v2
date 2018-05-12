f5c94959a94a5ba058ad504e9104ef3f2f86c890
Angus MacDonald <angusamacdonald@hotmail.com>
2018-05-11 23:29:55 -0400

    FrameMemoized getVisibleArea

--- src/toolbox/utils/dom/position/get-visible-area.ts
+++ src/toolbox/utils/dom/position/get-visible-area.ts
@@ -1,8 +1,9 @@
 import {getOpacity} from '../style/get-opacity';
 import {isDisplayed} from "../style/is-displayed";
 import {getVisibleDimensions} from "./get-visible-dimensions";
+import {frameMemoize} from "../../frame-memoize";
 
-function getVisibleArea(
+function getVisibleArea_(
   target: HTMLElement,
   container: HTMLElement = null,
   factorInOpacity: boolean = false
@@ -14,5 +15,7 @@ function getVisibleArea(
   const opacityFactor: number = factorInOpacity ? getOpacity(target) : 1;
   return getVisibleDimensions(target, container).getArea() * opacityFactor;
 }
+
+const getVisibleArea = frameMemoize(getVisibleArea_);
 
 export {getVisibleArea};
