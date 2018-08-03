2c11a1c93a9313ff35e1bf87b5d62ae6bdb9580e
Angus MacDonald <angusamacdonald@hotmail.com>
2018-08-03 19:58:59 -0400

    Updated basic video scrub to call back when video is loaded

--- src/toolbox/components/scroll-effect/effect-generators/basic-video-scrub.ts
+++ src/toolbox/components/scroll-effect/effect-generators/basic-video-scrub.ts
@@ -3,19 +3,32 @@ import {renderLoop} from "../../../utils/render-loop";
 import {percentToIndex} from "../../../utils/array/percent-to-index";
 import {setStyle} from "../../../utils/dom/style/set-style";
 
+const waitForLoadMap = new Map();
+
 function generateBasicVideoScrubEffect(): TScrollEffectEffectFunction {
   return function(
     target: HTMLElement, distance: number, distanceAsPercent: number
   ) {
     // Assume a video element
     const video = <HTMLMediaElement>target;
-    const duration = video.duration;
-    if (!isNaN(duration)) {
-      renderLoop.mutate(() => {
-        video.pause();
-        video.currentTime = duration * distanceAsPercent;
-      });
-    }
+    const updateVideo = () => {
+      const duration = video.duration;
+      if (!isNaN(duration)) {
+        waitForLoadMap.delete(video);
+        renderLoop.mutate(() => {
+          video.pause();
+          video.currentTime = duration * distanceAsPercent;
+        });
+      } else {
+        if (waitForLoadMap.has(video)) {
+          const oldListener = waitForLoadMap.get(video);
+          video.removeEventListener('loadeddata', oldListener);
+        }
+        waitForLoadMap.set(video, updateVideo);
+        video.addEventListener('loadeddata', updateVideo);
+      }
+    };
+    updateVideo();
   }
 }
 
