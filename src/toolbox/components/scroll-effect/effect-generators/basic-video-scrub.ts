88971bed0217d59982cdef2f1d47f6bb0503e6d3
Angus MacDonald <angusamacdonald@hotmail.com>
2018-08-03 20:17:39 -0400

    Added updateOnScrollDownOnly option to basicvideoscrub

--- src/toolbox/components/scroll-effect/effect-generators/basic-video-scrub.ts
+++ src/toolbox/components/scroll-effect/effect-generators/basic-video-scrub.ts
@@ -2,15 +2,29 @@ import {TScrollEffectEffectFunction} from "../types/scroll-effect-effect-functio
 import {renderLoop} from "../../../utils/render-loop";
 import {percentToIndex} from "../../../utils/array/percent-to-index";
 import {setStyle} from "../../../utils/dom/style/set-style";
+import {DynamicDefaultMap} from "../../../utils/map/dynamic-default";
 
 const waitForLoadMap = new Map();
+const lastDistancePercent =
+  DynamicDefaultMap.usingFunction((x: HTMLElement) => Number.MIN_VALUE);
 
-function generateBasicVideoScrubEffect(): TScrollEffectEffectFunction {
+function generateBasicVideoScrubEffect(
+  updateOnScrollDownOnly: boolean = true
+): TScrollEffectEffectFunction {
   return function(
     target: HTMLElement, distance: number, distanceAsPercent: number
   ) {
     // Assume a video element
     const video = <HTMLMediaElement>target;
+    if (
+      distanceAsPercent <= lastDistancePercent.get(video) &&
+      updateOnScrollDownOnly
+    ) {
+      return;
+    }
+
+    lastDistancePercent.set(video, distanceAsPercent);
+
     const updateVideo = () => {
       const duration = video.duration;
       if (!isNaN(duration)) {
