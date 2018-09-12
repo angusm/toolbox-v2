0c03e71d6ced9d9eee6556c93a32e090c12e0289
Angus MacDonald <angusamacdonald@hotmail.com>
2018-09-12 11:42:04 -0400

    Documented returnUntouched

--- src/toolbox/utils/return-untouched.ts
+++ src/toolbox/utils/return-untouched.ts
@@ -1,3 +1,8 @@
+/**
+ * Simple function that does nothing to its parameter.
+ * It is not more concise than (v: T) => v; but importing it and using it
+ * makes the intention clear as the naming provides documentation.
+ */
 function returnUntouched<T>(v: T): T {
   return v;
 }
