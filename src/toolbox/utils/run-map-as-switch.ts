3afbcf4fc2dc8d79f4c09e8fb21aea63bed6f450
Angus MacDonald <angusamacdonald@hotmail.com>
2018-09-12 11:40:33 -0400

    Documented runMapAsSwitch

--- src/toolbox/utils/run-map-as-switch.ts
+++ src/toolbox/utils/run-map-as-switch.ts
@@ -1,5 +1,39 @@
 import {defaultSymbol} from './default-symbol';
 
+/**
+ * Allows for maps of keys to functions to replace switches.
+ * - Removes risk of accidental fall through.
+ * - More concise syntax than if statements, and switch statements in some cases
+ *
+ * Uses Toolbox's default symbol as the key for the desired default case.
+ *
+ * So for instance:
+ *
+ * function someSwitchFn(value, a, b, c) {
+ *   switch (value) {
+ *     case 1:
+ *       return fnJ(a, b, c);
+ *     case 2:
+ *       return fnK(a, b, c);
+ *     case 'applesauce':
+ *       return fnL(a, b, c);
+ *     default:
+ *       return fnM(a, b, c);
+ *   }
+ * }
+ *
+ * can become...
+ *
+ * function someSwitchFn(value, a, b, c) {
+ *   const switchMap = new Map([
+ *     [1, fnJ],
+ *     [2, fnK],
+ *     ['applesauce', fnL],
+ *     [defaultSymbol, fnM],
+ *   ]);
+ *   return runMapAsSwitch(switchMap, value, a, b, c);
+ * }
+ */
 function runMapAsSwitch<K, V, R>(
   map: Map<K|symbol, (...any:V[]) => R>, key: K, ...args: Array<V>
 ): R {
