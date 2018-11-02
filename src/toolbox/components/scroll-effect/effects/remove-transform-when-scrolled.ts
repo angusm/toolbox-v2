ab9d2f36e92f9de1265639317e26fe6badb26b18
Angus MacDonald <angusamacdonald@hotmail.com>
2018-11-02 13:32:29 -0400

    Added RemoveTransformWhenScrolled effect

---
+++ src/toolbox/components/scroll-effect/effects/remove-transform-when-scrolled.ts
@@ -0,0 +1,30 @@
+import {IEffect} from "./ieffect";
+import {setStyle} from "../../../utils/dom/style/set-style";
+import {renderLoop} from "../../../utils/render-loop";
+
+// This can be used to only display a header on scroll down.
+// To set this up place a header with a transform that slides it up
+// and out of view. Then when scrolling down and the transform is removed the
+// header will be displayed.
+
+class RemoveTransformWhenScrolled implements IEffect {
+  private minimumScrollDistance_: number;
+
+  constructor(minimumScrollDistance: number = 0) {
+    this.minimumScrollDistance_ = minimumScrollDistance;
+  }
+
+  public run(
+    target: HTMLElement, distance: number, distanceAsPercent: number
+  ): void {
+    if (distance > this.minimumScrollDistance_) {
+      renderLoop.scrollMutate(() => setStyle(target, 'transform', 'none'));
+    } else {
+      renderLoop.scrollMutate(() => setStyle(target, 'transform', ''));
+    }
+  }
+
+  destroy() {}
+}
+
+export {RemoveTransformWhenScrolled};
