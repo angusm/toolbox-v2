343eb9dfa97315f4b72b2a7645ec47911f45775c
Angus MacDonald <angusamacdonald@hotmail.com>
2019-01-28 00:15:43 -0600

    Trigger RAF run after scroll event

--- src/toolbox/utils/render-loop.ts
+++ src/toolbox/utils/render-loop.ts
@@ -59,6 +59,7 @@ class RenderLoop {
 
   private msPerFrame_: number;
   private scheduledFns_: DynamicDefaultMap<symbol, RenderFunctionMap>;
+  private scrolledSinceLastFrame_: boolean;
 
   constructor() {
     this.scheduledFns_ =
@@ -66,6 +67,7 @@ class RenderLoop {
         .usingFunction<symbol, RenderFunctionMap>(
           (unused: symbol) => new Map<RenderFunctionID, RenderFunction>());
     this.msPerFrame_ = 33; // Default to 30fps
+    this.scrolledSinceLastFrame_ = false;
     [
       'mousewheel',
       'wheel',
@@ -142,6 +144,7 @@ class RenderLoop {
   }
 
   private runScrollLoop_(): void {
+    this.scrolledSinceLastFrame_ = true;
     this.runScrollFns_();
   }
 
@@ -150,7 +153,11 @@ class RenderLoop {
   }
 
   private runFns_(): void {
-    ANIMATION_FRAME_STEP_ORDER.forEach((step) => this.runFnsForStep_(step));
+    const stepOrder =
+      this.scrolledSinceLastFrame_ ?
+        ALL_STEP_ORDER : ANIMATION_FRAME_STEP_ORDER;
+    this.scrolledSinceLastFrame_ = false;
+    stepOrder.forEach((step) => this.runFnsForStep_(step));
   }
 
   private runFnsForStep_(step: symbol): void {
