6e2bdd9d2b580a297d48dd1da17fc70f875927ed
Angus MacDonald <angusamacdonald@hotmail.com>
2019-02-05 19:34:41 -0600

    Correct error in getVisibleDistanceBetweenElementCenters

--- src/toolbox/utils/dom/position/get-visible-distance-between-element-centers.ts
+++ src/toolbox/utils/dom/position/get-visible-distance-between-element-centers.ts
@@ -7,8 +7,8 @@ function getVisibleDistanceBetweenElementCenters(
   a: HTMLElement, b: HTMLElement
 ): Vector2d {
   const distance: Vector2d = getVisibleDistanceBetweenElements(a, b);
-  const elementSize: Dimensions2d = Dimensions2d.fromElementOffset(a);
-  let containerSize: Dimensions2d = getAncestorDimensions(b);
+  const elementSize: Dimensions2d = Dimensions2d.fromElementOffset(a).scale(.5);
+  let containerSize: Dimensions2d = getAncestorDimensions(b).scale(.5);
   return distance.add(Vector2d.fromVector(elementSize.subtract(containerSize)));
 }
 
