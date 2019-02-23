1ddfd04c5acd3ad935d59b968db0e6d4ea1e75f6
Kaden Zipfel <kadenzipfel@Kadens-MacBook-Pro.local>
2019-02-22 16:39:55 -0800

    Add more tests

--- src/toolbox/utils/return-untouched.spec.ts
+++ src/toolbox/utils/return-untouched.spec.ts
@@ -1,8 +1,15 @@
 import { returnUntouched } from "./return-untouched";
 import { expect } from "chai";
 import "mocha";
+import { areArrayValuesEqual } from "./array/are-array-values-equal";
 
-const tests = [[54321, 54321], ["abc", "abc"]];
+const tests = [
+  [54321, 54321],
+  ["abc", "abc"],
+  [true, true],
+  [null, null],
+  [undefined, undefined]
+];
 
 describe("returnUntouched", () => {
   tests.forEach(([testInput, expectedResult]) => {
@@ -11,5 +18,15 @@ describe("returnUntouched", () => {
     )}`, () => {
       expect(returnUntouched(testInput)).to.equal(expectedResult);
     });
+  });
+  it("should return identical arrays", () => {
+    const array = [1, 2, 3];
+    const array2 = [1, 2, 3];
+    expect(areArrayValuesEqual(array, array2)).to.equal(true);
+  });
+  it("should return identical objects", () => {
+    const obj = { a: 1, b: 2 };
+    const obj2 = { a: 1, b: 2 };
+    expect(JSON.stringify(obj)).to.equal(JSON.stringify(obj2));
   });
 });
