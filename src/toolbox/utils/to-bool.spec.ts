f5a745e1d79ed3d01987c7acf7c2a48d616ba4d7
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-02-22 16:54:15 -0800

    Add tests that return false

--- src/toolbox/utils/to-bool.spec.ts
+++ src/toolbox/utils/to-bool.spec.ts
@@ -2,7 +2,15 @@ import { toBool } from "./to-bool";
 import { expect } from "chai";
 import "mocha";
 
-const tests = [[1, true], [{}, true], [[], true], ["asdf", true]];
+const tests = [
+  [1, true],
+  [{}, true],
+  [[], true],
+  ["asdf", true],
+  [null, false],
+  [undefined, false],
+  [0, false]
+];
 
 describe("toBool", () => {
   tests.forEach(([testInput, expectedResult]) => {
