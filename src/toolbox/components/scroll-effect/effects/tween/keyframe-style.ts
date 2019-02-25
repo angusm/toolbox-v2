ef43d225ffb71127eb22a22f52acba8b9f3c36c6
Angus MacDonald <angusamacdonald@hotmail.com>
2019-02-24 22:00:44 -0600

    Documented KeyframeStyle

--- src/toolbox/components/scroll-effect/effects/tween/keyframe-style.ts
+++ src/toolbox/components/scroll-effect/effects/tween/keyframe-style.ts
@@ -1,5 +1,15 @@
+/**
+ * The different ways that Keyframe positions can be interpreted.
+ */
 enum KeyframeStyle {
+  /**
+   * Treat keyframe positions as representing a scroll position in pixels.
+   */
   PERCENT,
+
+  /**
+   * Treat keyframe positions as representing a percentage of a scroll range.
+   */
   PIXEL,
 }
 
