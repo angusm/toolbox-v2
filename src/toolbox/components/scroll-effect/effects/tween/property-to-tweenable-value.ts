dec845ee57b6fd17ed5f30efd5e4d359cbe022c7
Angus MacDonald <angusamacdonald@hotmail.com>
2019-02-24 21:55:37 -0600

    Documented some more ScrollEffect Tween files

--- src/toolbox/components/scroll-effect/effects/tween/property-to-tweenable-value.ts
+++ src/toolbox/components/scroll-effect/effects/tween/property-to-tweenable-value.ts
@@ -3,6 +3,14 @@ import {ITweenableValueStatic} from "./interfaces/tweenable-value";
 import {CssCalcFormula} from "../../../../utils/dom/style/css-calc-formula";
 import {CssNumber} from "../../../../utils/dom/style/css-number";
 
+/**
+ * Define Classes to use to represent the values of different CSS properties.
+ *
+ * Each property contained here should be tweenable and should be represented
+ * by a class that implements ITweenableValueStatic, which is to say that it
+ * implements both IMeasurableStatic<ITweenableValueInstance> and
+ * ICssStyleValueStatic<ITweenableValueInstance>.
+ */
 const propertyToTweenableValue =
   new Map<string, ITweenableValueStatic>([
     ['background-color', Color],
--- src/toolbox/components/scroll-effect/effects/tween/t-keyframes-config.ts
+++ src/toolbox/components/scroll-effect/effects/tween/t-keyframes-config.ts
@@ -1,3 +1,11 @@
+/**
+ * Defines the type used to identify a collection of keyframes.
+ *
+ * String values should be a CSS Style or set of CSS Styles, such as:
+ *
+ *     "opacity: 0"
+ *     "background: purple; transform: translateY(calc(3px + 10%))"
+ */
 type TKeyframesConfig = [number, string][];
 
 export {TKeyframesConfig};
