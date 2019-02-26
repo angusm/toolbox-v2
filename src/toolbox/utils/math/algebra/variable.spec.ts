0f4c00716bf3dd98d078faefa81c1078feb965de
Angus MacDonald <angusamacdonald@hotmail.com>
2019-02-26 17:28:13 -0600

    Added additional tests to Variable

--- src/toolbox/utils/math/algebra/variable.spec.ts
+++ src/toolbox/utils/math/algebra/variable.spec.ts
@@ -6,6 +6,12 @@ describe('Variable', () => {
   describe('fromString', () => {
     const tests: any[] = [
       ['5.9vh', '5.9vh'],
+      ['.9vh', '0.9vh'],
+      ['0.9vh', '0.9vh'],
+      ['1000.923vh', '1000.923vh'],
+      ['1000.9vh', '1000.9vh'],
+      ['9vh', '9vh'],
+      ['9.0vh', '9vh'],
     ];
     tests.forEach(([testInput, expectedResult]) => {
       it(`should return ${JSON.stringify(expectedResult)} for ${JSON.stringify(testInput)}`, () => {
