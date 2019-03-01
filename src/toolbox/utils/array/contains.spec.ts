479825a98c0221810623db83cb090aed4f1b9d99
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-03-01 12:03:54 -0800

    Add other types of values

--- src/toolbox/utils/array/contains.spec.ts
+++ src/toolbox/utils/array/contains.spec.ts
@@ -7,7 +7,11 @@ describe("contains", () => {
     [[1, 2, 3], 2, true],
     [[2, 4, 6], 5, false],
     [["cat", "dog", "rabbit"], "dog", true],
-    [["cat", "dog", "rabbit"], "goose", false]
+    [["cat", "dog", "rabbit"], "goose", false],
+    [["cat", null, 3], null, true],
+    [["cat", undefined, 3], undefined, true],
+    [["cat", true, 3], true, true],
+    [["cat", false, 3], false, true]
   ];
   tests.forEach(([inputArray, containedElement, expectedResult]) => {
     it(`should return ${JSON.stringify(
