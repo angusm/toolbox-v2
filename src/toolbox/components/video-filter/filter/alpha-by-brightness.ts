4042623d39fb31a61b8d27538f93111ffcd240c4
Angus MacDonald <angusamacdonald@hotmail.com>
2019-03-04 17:16:41 -0600

    Value range parameter to AlphaByBrightness filter

--- src/toolbox/components/video-filter/filter/alpha-by-brightness.ts
+++ src/toolbox/components/video-filter/filter/alpha-by-brightness.ts
@@ -1,7 +1,14 @@
 import {IFilter} from "../video-filter-interface";
+import {NumericRange} from "../../../utils/math/numeric-range";
+
+const DEFAULT_RANGE = new NumericRange(0, 255);
 
 class AlphaByBrightness implements IFilter {
-  constructor() {}
+  private readonly validRange_: NumericRange;
+
+  constructor(validRange: NumericRange = DEFAULT_RANGE) {
+    this.validRange_ = validRange;
+  }
 
   public filter(imageData: ImageData): ImageData {
     const colorValues = imageData.data;
@@ -9,7 +16,9 @@ class AlphaByBrightness implements IFilter {
     for (let i = 0; i < length; i +=4) {
       // Convert brightness to alpha and replace in-array
       colorValues[i+3] =
-          3 * colorValues[i] + 4 * colorValues[i+1] + colorValues[i+2] >>> 3;
+        255 *
+        this.validRange_.getValueAsPercent(
+          3 * colorValues[i] + 4 * colorValues[i+1] + colorValues[i+2] >>> 3);
     }
     return imageData;
   }
