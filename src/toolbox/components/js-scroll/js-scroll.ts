444419c488dcdb68fd107459198f10f6a58736fd
Angus MacDonald <angusamacdonald@hotmail.com>
2019-03-14 09:25:49 -0600

    Force active listener in jsScroll

--- src/toolbox/components/js-scroll/js-scroll.ts
+++ src/toolbox/components/js-scroll/js-scroll.ts
@@ -1,5 +1,11 @@
 import {Vector2d} from "../../utils/math/geometry/vector-2d";
 import {applyScroll} from "../../utils/dom/position/apply-scroll";
+import {UserAgent} from "../../utils/user-agent/user-agent";
+import {IE} from "../../utils/user-agent/browser/ie";
+
+const thirdEventListenerParam =
+  UserAgent.getBrowser() === IE ?
+    true : {passive: false, capture: true, once: false};
 
 class JsScroll {
   private readonly wheelHandler_: (e: WheelEvent) => void;
@@ -12,7 +18,8 @@ class JsScroll {
 
   public init(): void {
     if (this.initializationCount_ === 0) {
-      window.addEventListener('wheel', this.wheelHandler_)
+      window.addEventListener(
+        'wheel', this.wheelHandler_, thirdEventListenerParam);
     }
     this.initializationCount_++;
   }
@@ -25,7 +32,8 @@ class JsScroll {
   public destroy(): void {
     this.initializationCount_--;
     if (this.initializationCount_ === 0) {
-      window.removeEventListener('wheel', this.wheelHandler_);
+      window.removeEventListener(
+        'wheel', this.wheelHandler_, thirdEventListenerParam);
     }
   }
 }
