3628bb96cbe63854242351997ee24b4ee1e15a3e
Angus MacDonald <angusamacdonald@hotmail.com>
2019-04-18 09:02:59 -0600

    Add warning handler to styleStringToMap

--- src/toolbox/utils/dom/style/style-string-to-map.ts
+++ src/toolbox/utils/dom/style/style-string-to-map.ts
@@ -1,7 +1,28 @@
 import {trim} from "../../string/trim";
+import {contains} from "../../string/contains";
 
 function styleStringToMap(styleString: string): Map<string, string> {
+  if (typeof styleString !== 'string') { // Handle bad parameters
+    console.warn(
+      `A non-string value "${styleString}" (tyepof ${typeof styleString}) ` +
+      `was provided to styleStringToMap`);
+    return new Map<string, string>();
+  }
   return styleString.split(';')
+    .filter((propertyStylePair) => {
+      if (contains(propertyStylePair, ':')) {
+        return true;
+      } else {
+        // Don't make a fuss over empty strings as they could be trailing
+        // semi-colons
+        if (propertyStylePair !== '') {
+          console.warn(
+            `Invalid property-style pair provided to styleStringToMap ` +
+            `"${propertyStylePair}"`);
+        }
+        return false;
+      }
+    })
     .map((propertyStylePair) => propertyStylePair.split(':'))
     .map((pair) => pair.map((value) => trim(value, [' ', '\n', '\t'])))
     .reduce(
