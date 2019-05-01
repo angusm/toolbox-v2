b3819bb99473c1166b2512bb9941ed41b98d6f61
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-05-01 14:22:19 -0700

    Change load event to canplay

--- src/toolbox/utils/loading/load-video.ts
+++ src/toolbox/utils/loading/load-video.ts
@@ -6,7 +6,7 @@
 function loadVideo(videoUrl: string): Promise<HTMLVideoElement> {
   return new Promise((resolve, reject) => {
     const video = document.createElement('video');
-    video.addEventListener('load', () => resolve(video), { once: true });
+    video.addEventListener('canplay', () => resolve(video), { once: true });
     video.addEventListener('error', () => reject(video), { once: true });
     video.src = videoUrl;
   });
