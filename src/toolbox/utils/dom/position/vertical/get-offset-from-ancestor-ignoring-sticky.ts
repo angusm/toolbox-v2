16fd314ade43b8ee5c731e1754589499c8581b16
Angus MacDonald <angusamacdonald@hotmail.com>
2019-07-01 16:58:50 -0600

    Support positioned elements in "getOffsetFromAncestorIgnoringSticky"

--- src/toolbox/utils/dom/position/vertical/get-offset-from-ancestor-ignoring-sticky.ts
+++ src/toolbox/utils/dom/position/vertical/get-offset-from-ancestor-ignoring-sticky.ts
@@ -1,12 +1,17 @@
 import {getOffsetTopIgnoringSticky} from "./get-offset-top-ignoring-sticky";
+import {getStyle} from "../../style/get-style";
 
-const ignoredPositions = new Set(['fixed', 'absolute']);
+const ignoredPositions = new Set(['fixed', 'absolute', 'relative']);
 
 function getOffsetFromAncestorIgnoringSticky(
   element: HTMLElement, ancestor: HTMLElement = null
 ): number {
   if (!element || element === ancestor) {
     return 0;
+  } else if (ignoredPositions.has(getStyle(element, 'position'))) {
+    return getOffsetTopIgnoringSticky(element) +
+      getOffsetFromAncestorIgnoringSticky(
+        <HTMLElement>element.offsetParent, ancestor);
   } else {
     return getOffsetTopIgnoringSticky(element) +
       getOffsetFromAncestorIgnoringSticky(
