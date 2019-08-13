b3c8722ebd787116dd8ded9d200cf5c113dd7b12
Angus MacDonald <angusamacdonald@hotmail.com>
2019-08-12 21:23:55 -0600

    Added option to check parents to isDisplayed

--- src/toolbox/utils/dom/style/is-displayed.ts
+++ src/toolbox/utils/dom/style/is-displayed.ts
@@ -1,7 +1,12 @@
 import {getStyle} from "./get-style";
 
-function isDisplayed(element: Element): boolean {
-  return getStyle(element, 'display') !== 'none';
+function isDisplayed(element: Element, checkParents = false): boolean {
+  if (!checkParents) {
+    return getStyle(element, 'display') !== 'none';
+  } else {
+    return isDisplayed(element) &&
+      (!element.parentElement || isDisplayed(element.parentElement, true));
+  }
 }
 
 export {isDisplayed};
