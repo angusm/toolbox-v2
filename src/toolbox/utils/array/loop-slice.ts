35de365caf33f81a29c7a20a9314199fe4e4809c
Angus MacDonald <amacdonald@google.com>
2020-09-27 11:32:48 -0600

    Fix bug in loopSlice
    Fixed issue where end index wasnt being wrapped

--- src/toolbox/utils/array/loop-slice.ts
+++ src/toolbox/utils/array/loop-slice.ts
@@ -2,11 +2,12 @@ import {getSign} from "../math/get-sign";
 import {wrapIndex} from "./wrap-index";
 
 function loopSlice<T>(
-  values: T[], startIndex: number, endIndex: number, direction: number
+  values: T[], startIndex: number, rawEndIndex: number, direction: number
 ): T[] {
   const result: T[] = [];
   const length = values.length;
   const increment = getSign(direction);
+  const endIndex = wrapIndex(rawEndIndex, length);
   let index = wrapIndex(startIndex, length);
   while (index !== endIndex) {
     result.push(values[index]);
